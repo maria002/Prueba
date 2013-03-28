fcf9475a79e42d2d491990ff2f4c162a1981c880
Maria Elena <maria_morillo02@hotmail.com>
2013-03-28 14:31:29 -0300

    Test
    para aprender

---
+++ Test/src/test/Sistema.java
@@ -0,0 +1,11 @@
+package test;
+/**
+ *
+ * @author Maria Elena
+ */
+public class Sistema {
+    public static void main(String[] args) {
+        Test test = new Test();
+}
+    
+}
---
+++ Test/src/test/Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Maria Elena
+ */
+public class Test {
+    public Test(){
+        if (5 == 5) {
+            System.out.println("Es igual a 5");
+        }
+        else {
+            System.out.println("No es igual a 5");
+        }
+    }
+}
